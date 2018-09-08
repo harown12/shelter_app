4acf38dd77c139bdb1e9d6b796a277f5561c7638
harown12 <harown12@gmail.com>
2018-09-08 10:15:51 +0300

    Update

--- app/src/main/java/com/life/shelter/people/homeless/home.java
+++ app/src/main/java/com/life/shelter/people/homeless/home.java
@@ -2,25 +2,15 @@ package com.life.shelter.people.homeless;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.AutoCompleteTextView;
-import android.widget.ImageView;
-import android.widget.SearchView;
 
-import static com.life.shelter.people.homeless.R.id.fragment_address_search;
-
-public class home extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+public class home extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
